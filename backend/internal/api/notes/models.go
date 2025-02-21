ad5c7e0244f43270adb85610f511f93905740b10
theboysack <ehoang2@student.gsu.edu>
2025-02-21 18:49:28 -0500

    model definition for notes

--- backend/internal/api/notes/models.go
+++ backend/internal/api/notes/models.go
@@ -1,5 +1,21 @@
 package notes
 
+import (
+	"encoding/json"
+
+	"github.com/theEricHoang/lovenote/backend/internal/api/users"
+)
+
 type Note struct {
-	Id uint `json:"id"`
+	Id        uint       `json:"id"`
+	Author    users.User `json:"author"`
+	Title     string     `json:"title"`
+	Content   string     `json:"content"`
+	PositionX float32    `json:"position_x"`
+	PositionY float32    `json:"position_y"`
+	CreatedAt string     `json:"created_at"`
+}
+
+func (n *Note) ToJSON(view string) ([]byte, error) {
+	return json.Marshal(n)
 }
