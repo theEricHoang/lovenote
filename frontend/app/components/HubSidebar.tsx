f76cebff9467fab5ab7a11deae769aae13c1f0ac
theboysack <ehoang2@student.gsu.edu>
2025-04-08 14:48:40 -0400

    user relationships properly load,error handling and loading states WIP

--- frontend/app/components/HubSidebar.tsx
+++ frontend/app/components/HubSidebar.tsx
@@ -1,5 +1,8 @@
 import { NavLink } from "react-router";
 import Sidebar from "./ui/Sidebar";
+import { CircleFadingPlus } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "~/lib/http";
 
 const mockData = [
   {
@@ -19,22 +22,52 @@ const mockData = [
   },
 ];
 
+interface Relationship {
+  id: number;
+  name: string;
+  picture: string;
+  created_at: string;
+}
+
 export default function HubSidebar() {
+  const { data } = useQuery({
+    queryKey: ['relationships'],
+    queryFn: async (): Promise<Array<Relationship>> => {
+      const response = await api.get("/relationships");
+      return response.data;
+    },
+  })
+
   return (
     <Sidebar>
-      <ul>
-        <li>
-          <NavLink className="w-full p-2 bg-gray-500" to="/">
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="w-full p-2 bg-gray-500" to="/">
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="w-full p-2 bg-gray-500" to="/">
-          </NavLink>
-        </li>
+      <button
+        className="flex w-full p-2 rounded-md hover:bg-gray-50 text-black font-medium items-center"
+      >
+        <CircleFadingPlus
+          size={40}
+        />
+        <span
+          className="mx-2"
+        >
+          new relationship
+        </span>
+      </button>
+
+      <ul className="space-y-1">
+        {data?.map((relationship: Relationship) => (
+          <li key={relationship.id}>
+            <NavLink className="flex p-2 rounded-md hover:bg-gray-50 text-black font-medium items-center" to="/">
+              <img
+                src={relationship.picture}
+                alt={`${relationship.name} icon`}
+                className="w-10 h-10 rounded-full"
+              />
+              <span className="mx-2">
+                {relationship.name}
+              </span>
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </Sidebar>
   );
